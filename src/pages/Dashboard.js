824c8a85af17c8f03072a6e858f519d3f4c5230b
Jakub Szpil <kuba0122@gmail.com>
2020-06-18 17:13:47 +0200

    modyfying

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
+import { StackedBarChart as Chart } from '@carbon/charts-react';
 import {
+  Link,
+  Modal,
   DataTable,
   TableContainer,
   Table,
@@ -10,8 +14,11 @@ import {
   TableBody,
   TableCell,
   TableToolbar,
+  TableExpandRow,
+  TableExpandedRow,
   TableBatchAction,
   TableBatchActions,
+  TableExpandHeader,
   TableToolbarAction,
   TableToolbarContent,
   TableSelectAll,
@@ -19,51 +26,142 @@ import {
   TableToolbarMenu,
   TableToolbarSearch,
   Button,
+  ModalBody,
+  ModalHeader,
 } from 'carbon-components-react';
-import { Run32 as RunIcon } from '@carbon/icons-react';
+import {
+  Run32 as RunIcon,
+  SkillLevelAdvanced16 as HighLevel,
+  SkillLevelIntermediate16 as MediumLevel,
+  SkillLevelBasic16 as LowLevel,
+  SkillLevel16 as BasicLevel,
+} from '@carbon/icons-react';
 
-function getHeaders(item = {}) {
-  const capitalize = (name) => name.charAt(0).toUpperCase() + name.slice(1);
-  return Object.keys(item)
-    .filter((item) => item !== 'id')
-    .map((key) => ({
-      header: capitalize(key),
-      key: key,
-    }));
-}
+// initial headers of table
+const initialHeaders = [
+  'Name',
+  'Url',
+  'Tag',
+  'OSS',
+  'VA',
+  'Static',
+].map((item) => ({ key: item.toLowerCase(), header: item }));
 
+// function that getting images from api from json and returning them in array
 function getImages(data) {
-  const imgs = [];
+  let imgs = [];
   let counter = 0;
   Object.entries(data).forEach(([releaseKey, release]) => {
     Object.entries(release).forEach(([componentKey, component]) => {
       const set_of_images = component['images'];
       set_of_images.forEach((image) => {
-        imgs.push({ id: `${counter}`, ...image });
+        imgs.push({
+          id: `${counter}`,
+          name: image?.name || '',
+          url: image?.url || '',
+          tag: image?.tag || '',
+          scanLinks: image?.scanLinks || [],
+          oss: image?.results?.oss || {},
+          va: image?.results?.va || {},
+          static: image?.results?.static || {},
+          results: image?.results || [],
+        });
         counter++;
       });
     });
   });
-  console.log(imgs);
   return imgs;
 }
 
-const fetchAPI = async (setter, api) => {
-  try {
-    const response = await fetch(api);
-    const result = await response.json();
-    setter(getImages(result));
-  } catch (e) {
-    setter({ error: `${e}` });
+// components
+const Level = (props) => {
+  switch (props.level) {
+    case 'low':
+      return <LowLevel {...props} fill="yellow" />;
+    case 'medium':
+      return <MediumLevel {...props} fill="orange" />;
+    case 'high':
+      return <HighLevel {...props} fill="red" />;
+    default:
+      return <BasicLevel {...props} />;
   }
 };
+const ChartModal = ({ data, handleChartClose, opened }) => {
+  const options = {
+    title: `Vulnerability severity levels of image ${data.name} `,
+    axes: {
+      left: {
+        mapsTo: 'value',
+        stacked: true,
+      },
+      bottom: {
+        mapsTo: 'key',
+        scaleType: 'labels',
+      },
+    },
+    height: '400px',
+  };
+  const chartData = Object.entries(data)
+    .filter(([key]) => key !== 'name')
+    .reduce((acc, [typeKey, typeValue]) => {
+      const val = Object.entries(typeValue).reduce(
+        (levelAcc, [levelKey, levelValue]) => {
+          return [
+            ...levelAcc,
+            {
+              key: `${typeKey}`,
+              group: `${levelKey}`,
+              value: levelValue,
+            },
+          ];
+        },
+        []
+      );
+      return [...acc, ...val];
+    }, []);
+  return (
+    <Modal
+      open={opened}
+      onBlurCapture={handleChartClose}
+      primaryButtonText={'OK'}
+      secondaryButtonText={'Close'}
+      onRequestClose={handleChartClose}
+      onRequestSubmit={handleChartClose}
+    >
+      <Chart data={chartData} options={options} />
+    </Modal>
+  );
+};
 
-const triggerFunction = async (type = String, selected = Array) => {
-  const conf = window.confirm(`Are sure to trigger ${type}?`);
-  if (conf) {
+function Dashboard() {
+  const [data, setData] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [show, setShow] = useState(false);
+  const [blur, setBlur] = useState(true);
+
+  const [action, setAction] = useState({
+    type: null,
+    selected: [],
+  });
+
+  const [chart, setChart] = useState({
+    opened: false,
+    data: {},
+  });
+
+  const fetchAPI = async (api) => {
+    try {
+      const response = await fetch(api);
+      const result = await response.json();
+      setData(getImages(result));
+    } catch (e) {
+      setData({ error: e });
+    }
+  };
+  const processAction = async () => {
     const response = await axios.post(
-      'http://192.168.99.100:8000/scan/' + type,
-      selected.map(({ cells = Array }) => {
+      'http://192.168.99.100:8000/scan/' + action.type,
+      action.selected.map(({ cells = Array }) => {
         let row = {};
         cells.forEach((cell) => {
           const rowName = cell['id'].split(':')[1];
@@ -74,26 +172,91 @@ const triggerFunction = async (type = String, selected = Array) => {
       })
     );
     const images = await response.data;
-    images.forEach(({ name, send, type }) => {
-      alert(`Image ${name} has${send ? '' : ' not'} been sent`);
-    });
-    console.log(images);
-  }
-};
+    alert(images);
+  };
 
-export default function () {
-  const [data, setData] = useState([]);
-  const [headers, setHeaders] = useState([]);
   useEffect(() => {
-    fetchAPI(setData, 'http://192.168.99.100:8000/');
+    const fAPI = fetchAPI('http://192.168.99.100:8000/');
+    setInterval(() => fAPI, 5000);
+    return () => clearInterval(() => fAPI);
   }, []);
   useEffect(() => {
-    setHeaders(getHeaders(data[0]));
+    setHeaders(initialHeaders);
   }, [data]);
+
+  const handleSubmit = () => {
+    setBlur(false);
+    processAction();
+    setShow(false);
+    setBlur(true);
+  };
+
+  const handleReject = () => (blur ? setShow(false) : null);
+  const triggerFunction = (type = String, selected = Array) => {
+    setShow(true);
+    setAction({ type: type, selected: selected });
+  };
+  const handleClose = () => {
+    return new Promise((resolve, reject) => {
+      if (blur) resolve(setShow(false));
+    });
+  };
+
+  const handleChartOpen = (item) => {
+    const [firstItem] = item.cells;
+    const { value } = firstItem;
+    const found = data.find((item) => item.name === value);
+
+    setChart({
+      opened: true,
+      data: {
+        name: found.name,
+        oss: found.oss,
+        va: found.va,
+        static: found.static,
+      },
+    });
+  };
+  const handleChartClose = () => {
+    setChart({
+      opened: false,
+      data: {},
+    });
+  };
+
   return (
     <div>
+      {show && (
+        <Modal
+          open={show}
+          onRequestClose={handleReject}
+          onSecondarySubmit={handleReject}
+          onRequestSubmit={handleSubmit}
+          primaryButtonText="Confirm"
+          secondaryButtonText="Cancel"
+          onBlurCapture={() => setTimeout(handleClose, 200)}
+        >
+          <ModalHeader title="Confirm action" />
+          <ModalBody>
+            Are you sure to trigger {action.type} scan?
+            <ul>
+              {action.selected.map((item, key) => (
+                <li key={key}>{item.name}</li>
+              ))}
+            </ul>
+          </ModalBody>
+        </Modal>
+      )}
+      {chart.opened && (
+        <ChartModal
+          data={chart.data}
+          handleChartClose={handleChartClose}
+          opened={chart.opened}
+        />
+      )}
       {data !== [] && (
         <DataTable
+          isSortable
           rows={data}
           headers={headers}
           render={({
@@ -106,7 +269,7 @@ export default function () {
             onInputChange,
             selectedRows,
           }) => (
-            <TableContainer title="DataTable with batch actions">
+            <TableContainer title="Security scans review">
               <TableToolbar>
                 <TableBatchActions {...getBatchActionProps()}>
                   <TableBatchAction
@@ -178,6 +341,7 @@ export default function () {
                 <TableHead>
                   <TableRow>
                     <TableSelectAll {...getSelectionProps()} />
+                    <TableExpandHeader />
                     {headers.map((header) => (
                       <TableHeader {...getHeaderProps({ header })}>
                         {header.header}
@@ -186,13 +350,95 @@ export default function () {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {rows.map((row) => (
-                    <TableRow {...getRowProps({ row })}>
-                      <TableSelectRow {...getSelectionProps({ row })} />
-                      {row.cells.map((cell) => (
-                        <TableCell key={cell.id}>{cell.value}</TableCell>
-                      ))}
-                    </TableRow>
+                  {rows.map((row, rowKey) => (
+                    <React.Fragment key={row.id}>
+                      <TableExpandRow {...getRowProps({ row })}>
+                        <TableSelectRow {...getSelectionProps({ row })} />
+                        {row.cells.map((cell) => {
+                          const isLevel = () =>
+                            ['oss', 'va', 'static'].find(
+                              (item) => item === cell.id.slice(2)
+                            );
+                          if (isLevel()) {
+                            const obj = cell.value;
+                            if (obj) {
+                              let currentLevel = '';
+                              if (obj.high > 0) currentLevel = 'high';
+                              else if (obj.medium > 0) currentLevel = 'medium';
+                              else if (obj.low > 0) currentLevel = 'low';
+                              else currentLevel = 'basic';
+                              return (
+                                <TableCell
+                                  key={cell.id}
+                                  onClick={
+                                    currentLevel !== 'basic'
+                                      ? () => handleChartOpen(row)
+                                      : null
+                                  }
+                                >
+                                  <Level
+                                    level={currentLevel}
+                                    style={
+                                      currentLevel !== 'basic'
+                                        ? { cursor: 'pointer' }
+                                        : null
+                                    }
+                                  />
+                                </TableCell>
+                              );
+                            }
+                          }
+                          return (
+                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                          );
+                        })}
+                      </TableExpandRow>
+                      {row.isExpanded && data[rowKey].scanLinks.length > 0 && (
+                        <TableExpandedRow colSpan={headers.length + 2}>
+                          <div
+                            style={{
+                              display: 'flex',
+                              alignItems: 'flex-start',
+                              justifyContent: 'flex-start',
+                            }}
+                          >
+                            {Object.entries(data[rowKey].results).map(
+                              ([resultKey, resultValue], actionKey) => {
+                                return (
+                                  <div
+                                    key={resultKey + rowKey}
+                                    style={{
+                                      display: 'flex',
+                                      flexDirection: 'column',
+                                      alignItems: 'flex-start',
+                                      justifyContent: 'flex-start',
+                                      marginRight: 30,
+                                    }}
+                                  >
+                                    <p style={{ marginBottom: 10 }}>
+                                      {resultKey}
+                                    </p>
+                                    {Object.entries(
+                                      data[rowKey].scanLinks[actionKey][
+                                        resultKey
+                                      ]
+                                    ).map(([linkKey, linkValue]) => (
+                                      <Link
+                                        href={linkValue}
+                                        target="_blank"
+                                        key={linkKey}
+                                      >
+                                        {linkKey}
+                                      </Link>
+                                    ))}
+                                  </div>
+                                );
+                              }
+                            )}
+                          </div>
+                        </TableExpandedRow>
+                      )}
+                    </React.Fragment>
                   ))}
                 </TableBody>
               </Table>
@@ -203,3 +449,5 @@ export default function () {
     </div>
   );
 }
+
+export default Dashboard;
